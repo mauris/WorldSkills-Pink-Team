8f72496254da3ee5496c06309fff1ff0a28a2c47
Felipe Marcos <felipemarcosdos@gmail.com>
2015-08-13 14:47:56 -0300

    Add Home and Match Controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,52 +1,35 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, nearestNeighbor) {
-  var items = [
-    { name: "Bill", age: 10, pc: "Mac"},
-    { name: "Bill", age: 10, pc: "Mac"},
-    { name: "Bill", age: 10, pc: "Mac"},
-    { name: "Bob", age: 10, pc: "Mac"},
-    { name: "Bob", age: 11, pc: ""},
-  ];
+.controller('HomeController', function($scope) {
 
-  var query = { name: "Bob", age: 12, pc: "Windows" };
+})
+
+.controller('MatchController', function($scope, $filter, nearestNeighbor, users) {
+  console.log(users);
+
+  var query = {
+    gender: 'male',
+    location: {lat:-23.52677, lng:-46.664291},
+    status: 'married',
+    typeOfIssue: 'Cervical',
+    treatment: 'Surgery',
+    numberOfChildren: 1,
+    role: 'fighter'
+  };
 
   var fields = [
-    { name: "name", measure: nearestNeighbor.comparisonMethods.word },
-    { name: "age", measure: nearestNeighbor.comparisonMethods.number, max: 100 },
-    { name: "pc", measure: nearestNeighbor.comparisonMethods.word }
+    {name: 'gender', measure: nearestNeighbor.comparisonMethods.word},
+    {name: 'status', measure: nearestNeighbor.comparisonMethods.word},
+    {name: 'typeOfIssue', measure: nearestNeighbor.comparisonMethods.word},
+    {name: 'treatment', measure: nearestNeighbor.comparisonMethods.word},
+    {name: 'numberOfChildren', measure: nearestNeighbor.comparisonMethods.number, max: 100}
   ];
 
-  nearestNeighbor.findMostSimilar(query, items, fields, function(nearest) {
+  nearestNeighbor.findMostSimilar(query, users, fields, function(nearest) {
     console.log('=== Query ===');
     console.log(query);
 
     console.log('=== Results ===');
-    console.log(nearest);
+    console.log( $filter('orderBy')(nearest, 'similarity', true) );
   });
 })
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
